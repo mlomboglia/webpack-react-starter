266a71be9b80c09001b2f295c80f22409919f3bf
Joe Coyle <jcoyle37@gmail.com>
2021-04-27 13:28:20 -0400

    format class names to support CSS Modules

--- src/index.js
+++ src/index.js
@@ -6,12 +6,11 @@ import './index.css';
 
 const App = () => {
   return (
-    <div>
-        <div>Welcome to my-webpack-react-starter</div>
-        <img src={MyImage} />
+    <div styleName="contentContainer">
+      <div>Welcome to my-webpack-react-starter</div>
+      <img src={MyImage} />
     </div>
   );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
-
